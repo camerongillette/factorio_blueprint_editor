db62ff8212f3d6d932be332f7874dbfddbabc149
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-11-22 08:31:58 -0500

    enable some rules

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": [ "off" ],
+        "indent": [ "error", 4 ],
+        "linebreak-style": [ "error", "windows" ],
+        "semi": [ "error", "always" ],
+        "eol-last": [ "error", "always" ],
+    }
+};
