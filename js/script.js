ac1b584767db7a5caf44371bc6008aaf1dd791c5
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-12-01 07:48:25 -0500

    require curly braces

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 window.onload = function () {
     document.addEventListener('keypress', function (e) {
         var key = e.which || e.keyCode;
-        
+
         if (key === 114) { // 114 is r
             rotatePreview();
         }
@@ -195,18 +195,23 @@ function createEntitiesFromJSON(jsonobj){
 
 // https://stackoverflow.com/questions/5999118/how-can-i-add-or-update-a-query-string-parameter
 function UpdateQueryString(key, value, url) {
-    if (!url) url = window.location.href;
+    if (!url) { 
+        url = window.location.href; 
+    }
+
     var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
         hash;
 
     if (re.test(url)) {
-        if (typeof value !== 'undefined' && value !== null)
+        if (typeof value !== 'undefined' && value !== null) {
             return url.replace(re, '$1' + key + "=" + value + '$2$3');
+        }
         else {
             hash = url.split('#');
             url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
-            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
                 url += '#' + hash[1];
+            }
             return url;
         }
     }
@@ -215,12 +220,14 @@ function UpdateQueryString(key, value, url) {
             var separator = url.indexOf('?') !== -1 ? '&' : '?';
             hash = url.split('#');
             url = hash[0] + separator + key + '=' + value;
-            if (typeof hash[1] !== 'undefined' && hash[1] !== null) 
+            if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
                 url += '#' + hash[1];
+            }
             return url;
         }
-        else
+        else {
             return url;
+        }
     }
 }
 
