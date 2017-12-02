eb9051081765eb068ebcf3b36e39071d45d4b9b5
Stephen A. Wilson <Stephen-356@hotmail.com>
2017-12-02 18:44:46 -0500

    fix lint errors

--- js/script.js
+++ js/script.js
@@ -535,10 +535,10 @@ function clearTile(tile){
 }
 
 function tileClick(event, tile) {
-    if(event.buttons == 2){  //right mouse button
+    if(event.buttons === 2){  //right mouse button
         clearTile(tile);
     }
-    else if(event.buttons == 1 && !previewIsEmpty()){ //left mouse button and item is selected
+    else if(event.buttons === 1 && !previewIsEmpty()){ //left mouse button and item is selected
         setTile(tile);
     }
 }
